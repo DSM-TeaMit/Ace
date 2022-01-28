34f570e42e2588b323213d7367fd3be6c871a4f9
pjp0704 <junhyeong25@gmail.com>
2022-01-28 23:24:25 +0900

    🍫 (#3) - Init TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { connectionOptions } from './ormconfig';
+import { Admin } from './shared/entities/admin/admin.entity';
+import { Comment } from './shared/entities/comment/comment.entity';
+import { Member } from './shared/entities/member/member.entity';
+import { Plan } from './shared/entities/plan/plan.entity';
+import { Project } from './shared/entities/project/project.entity';
+import { Report } from './shared/entities/report/report.entity';
+import { Status } from './shared/entities/status/status.entity';
+import { User } from './shared/entities/user/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...connectionOptions[process.env.NODE_ENV],
+      entities: [Admin, Comment, Member, Plan, Project, Report, Status, User],
+      namingStrategy: new SnakeNamingStrategy(),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
