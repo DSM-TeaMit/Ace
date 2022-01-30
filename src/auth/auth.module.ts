affccbf7aa6edd81f553a22dc99886a45091d3c5
pjp0704 <junhyeong25@gmail.com>
2022-01-31 00:31:00 +0900

    🍫 (#7) - Redis connection

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,17 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      ttl: 604800,
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
