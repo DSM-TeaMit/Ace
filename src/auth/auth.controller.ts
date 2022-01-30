1d147b7b32aa5bebe06866493f9fd1b82a08167a
pjp0704 <junhyeong25@gmail.com>
2022-01-31 02:41:33 +0900

    🍫 (#7) - Google login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
 import { AuthService } from './auth.service';
+import { GoogleOauthGuard } from './guards/google-oauth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Get()
+  @UseGuards(GoogleOauthGuard)
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async googleAuth() {}
+
+  @Get('callback')
+  @UseGuards(GoogleOauthGuard)
+  async googleAuthRedirect(@Req() req: Request) {
+    return req.user;
+  }
 }
