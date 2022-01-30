ebd4ce0201d4a75b109ef20dc7d65f42e0a2aba1
pjp0704 <junhyeong25@gmail.com>
2022-01-31 02:44:57 +0900

    🍫 (#7) - Add JwtModule.register and JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,8 @@ import * as redisStore from 'cache-manager-redis-store';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { GoogleOauthStrategy } from './strategies/google-oauth.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
@@ -12,8 +14,12 @@ import { GoogleOauthStrategy } from './strategies/google-oauth.strategy';
       port: process.env.REDIS_PORT,
       ttl: 604800,
     }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, GoogleOauthStrategy],
+  providers: [AuthService, GoogleOauthStrategy, JwtStrategy],
 })
 export class AuthModule {}
