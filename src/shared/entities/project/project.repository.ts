81a9e2448b3d4509f89acf8f0dfbd9cf8270c6c2
pjp0704 <junhyeong25@gmail.com>
2022-02-03 18:33:17 +0900

    🍫 (#11) - Get projects of user

--- src/shared/entities/project/project.repository.ts
+++ src/shared/entities/project/project.repository.ts
@@ -2,4 +2,21 @@ import { AbstractRepository, EntityRepository } from 'typeorm';
 import { Project } from './project.entity';
 
 @EntityRepository(Project)
-export class ProjectRepository extends AbstractRepository<Project> {}
+export class ProjectRepository extends AbstractRepository<Project> {
+  async getProjectsOfUser(userId: number, isMine: boolean) {
+    const qb = this.createQueryBuilder('project')
+      .select()
+      .leftJoinAndSelect('project.members', 'members')
+      .leftJoinAndSelect('members.userId', 'userId')
+      .where('members.userId = :userId', { userId })
+      .orderBy('project.createdAt', 'DESC')
+      .take(4);
+    if (!isMine)
+      qb.leftJoinAndSelect('project.status', 'status').andWhere(
+        'report.isReportAccepted = true',
+      );
+
+    return qb.getManyAndCount();
+  }
+
+}
