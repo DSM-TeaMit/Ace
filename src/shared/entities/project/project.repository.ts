57ab2872f9c255ca839039903427c68dcf616604
pjp0704 <junhyeong25@gmail.com>
2022-02-04 17:48:20 +0900

    🍫 (#11) - Get pending projects

--- src/shared/entities/project/project.repository.ts
+++ src/shared/entities/project/project.repository.ts
@@ -19,4 +19,26 @@ export class ProjectRepository extends AbstractRepository<Project> {
     return qb.getManyAndCount();
   }
 
+  async getPendingProjects(userId?: number) {
+    const qb = this.createQueryBuilder('project')
+      .select()
+      .leftJoinAndSelect('project.status', 'status')
+      .leftJoinAndSelect('project.members', 'members')
+      .where((qb) => {
+        qb.where((qb) => {
+          qb.where('status.isPlanSubmitted = true').andWhere(
+            'status.isPlanAccepted IS NULL',
+          );
+        });
+        qb.orWhere((qb) => {
+          qb.where('status.isReportSubmitted = true').andWhere(
+            'status.isReportAccepted IS NULL',
+          );
+        });
+      })
+      .take(4);
+    if (userId) qb.andWhere('members.userId = :userId', { userId });
+
+    return qb.getManyAndCount();
+  }
 }
