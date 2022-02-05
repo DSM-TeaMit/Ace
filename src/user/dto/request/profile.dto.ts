69424bfd73e7cdd50dfe357a8b7fbaf47cc3220e
pjp0704 <junhyeong25@gmail.com>
2022-02-05 14:46:47 +0900

    ➕️ (#11) - Request query DTO

--- src/user/dto/request/profile.dto.ts
+++ src/user/dto/request/profile.dto.ts
@@ -1,7 +1,20 @@
-import { IsOptional, IsUUID } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, IsUUID, Min } from 'class-validator';
 
 export class ProfileRequestDto {
   @IsUUID('4')
   @IsOptional()
   uuid?: string;
+}
+
+export class ProfileRequestQueryDto {
+  @IsNumber()
+  @Min(1)
+  @Type(() => Number)
+  page: number;
+
+  @IsNumber()
+  @Min(1)
+  @Type(() => Number)
+  limit: number;
 }
