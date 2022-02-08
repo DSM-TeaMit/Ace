a24e9a1c36a535eb88a77b7dacf39a474e4ef294
pjp0704 <junhyeong25@gmail.com>
2022-02-08 14:45:08 +0900

    ➕️ (#12) - Response DTO

---
+++ src/comment/dto/response/get-comment.dto.ts
@@ -0,0 +1,12 @@
+export interface GetCommentResponseDto {
+  count: number;
+  comments: Comment[];
+}
+
+interface Comment {
+  writerId: string;
+  writerType: 'user' | 'admin';
+  writerName: string;
+  writerSno?: number;
+  content: string;
+}
