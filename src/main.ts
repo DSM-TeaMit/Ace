b23873bc8e26976f2498f73a016063460da57dff
pjp0704 <junhyeong25@gmail.com>
2022-02-14 21:19:27 +0900

    🍫 (#22) - Add trust proxy setting

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { LoggerInterceptor } from './shared/interceptors/logger.interceptor';
+import 'dotenv/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.set('trust proxy', true);
+  }
 
   app.use(helmet());
 
