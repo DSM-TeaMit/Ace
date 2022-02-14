5c66280d846f1f686aa472ec8dff45c3574e8041
pjp0704 <junhyeong25@gmail.com>
2022-02-14 19:23:22 +0900

    😎 - Missing imports

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -4,9 +4,18 @@ import { CommentController } from './comment.controller';
 import { CommentRepository } from 'src/shared/entities/comment/comment.repository';
 import { ProjectRepository } from 'src/shared/entities/project/project.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminRepository } from 'src/shared/entities/admin/admin.repository';
+import { UserRepository } from 'src/shared/entities/user/user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentRepository, ProjectRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      AdminRepository,
+      CommentRepository,
+      ProjectRepository,
+      UserRepository,
+    ]),
+  ],
   providers: [CommentService],
   controllers: [CommentController],
 })
