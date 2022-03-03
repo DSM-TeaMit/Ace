8276f7354523b826ca337b7dbc603687208e2648
pjp0704 <junhyeong25@gmail.com>
2022-03-03 18:57:41 +0900

    💫 (#51) - Update response DTO

--- src/auth/dto/response/login.dto.ts
+++ src/auth/dto/response/login.dto.ts
@@ -1,5 +1,8 @@
 export interface LoginResponseDto {
   type?: 'registration' | 'login';
+  studentNo?: number;
+  name: string;
+  userType: 'user' | 'admin';
   accessToken: string;
   refreshToken?: string;
   uuid?: string;
