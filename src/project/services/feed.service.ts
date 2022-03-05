5361f0f10c4fa7e681f54a61ca68282075507d9c
pjp0704 <junhyeong25@gmail.com>
2022-03-05 21:20:20 +0900

    🍫 (#52) - getPendingProjects

--- src/project/services/feed.service.ts
+++ src/project/services/feed.service.ts
@@ -3,6 +3,7 @@ import { ProjectRepository } from 'src/shared/entities/project/project.repositor
 import { FeedRequestDto } from '../dto/request/feed.dto';
 import { SearchRequestDto } from '../dto/request/search.dto';
 import { FeedResponseDto } from '../dto/response/feed.dto';
+import { PendingProjectDto } from '../dto/response/pending-project.dto';
 
 @Injectable()
 export class FeedService {
@@ -39,6 +40,33 @@ export class FeedService {
         projectField: project.field,
         viewCount: project.viewCount,
       })),
+    };
+  }
+
+  async getPendingProjects(
+    query: Omit<FeedRequestDto, 'order'>,
+  ): Promise<PendingProjectDto> {
+    const projects = await this.projectRepository.getPendingProjects(query);
+
+    return {
+      count: projects[1],
+      projects: projects[0].map((project) => {
+        const { status } = project;
+        const isPlanOrReport =
+          status.isPlanSubmitted === true && status.isPlanAccepted !== true
+            ? 'plan'
+            : 'report';
+        return {
+          projectName: project.projectName,
+          projectType: project.projectType,
+          reportType: isPlanOrReport.toUpperCase() as 'PLAN' | 'REPORT',
+          submittedAt: project.status[`${isPlanOrReport}SubmittedAt`],
+          writer: {
+            studentNo: project.writerId.studentNo,
+            name: project.writerId.name,
+          },
+        };
+      }),
     };
   }
 }
