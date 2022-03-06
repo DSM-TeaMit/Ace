6090e833af07b256a3853c2c5a95c4a096954e59
pjp0704 <junhyeong25@gmail.com>
2022-03-06 20:59:59 +0900

    ➕️ (#63) - Request DTO

--- src/project/dto/request/search.dto.ts
+++ src/project/dto/request/search.dto.ts
@@ -1,7 +1,12 @@
-import { IsString } from 'class-validator';
+import { IsEnum, IsString } from 'class-validator';
 import { FeedRequestDto } from './feed.dto';
 
 export class SearchRequestDto extends FeedRequestDto {
   @IsString()
   keyword: string;
+}
+
+export class SearchTypeRequestDto extends SearchRequestDto {
+  @IsEnum(['projectName', 'memberName'])
+  searchBy: 'projectName' | 'memberName';
 }
