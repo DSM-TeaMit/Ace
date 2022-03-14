9e8695e742e0c7774f2bba95a6fb166b8dc725f8
pjp0704 <junhyeong25@gmail.com>
2022-03-14 20:07:21 +0900

    😎 (#47) - Handle code error by extending guard

--- src/auth/guards/github-oauth.guard.ts
+++ src/auth/guards/github-oauth.guard.ts
@@ -1,5 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class GithubOauthGuard extends AuthGuard('github') {}
+export class GithubOauthGuard extends AuthGuard('github') {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  handleRequest(err: Error, user: any) {
+    if (err) {
+      if (err.message === 'The code passed is incorrect or expired.')
+        throw new UnauthorizedException();
+      else throw err;
+    }
+    return user;
+  }
+}
--- src/auth/guards/google-oauth.guard.ts
+++ src/auth/guards/google-oauth.guard.ts
@@ -1,5 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class GoogleOauthGuard extends AuthGuard('google') {}
+export class GoogleOauthGuard extends AuthGuard('google') {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  handleRequest(err: Error, user: any) {
+    if (err) {
+      if (err.message === 'Bad Request') throw new UnauthorizedException();
+      else throw err;
+    }
+    return user;
+  }
+}
