9211b117c6dedc619b7034d8bf64ba3f4cfce43c
pjp0704 <junhyeong25@gmail.com>
2022-03-30 21:01:57 +0900

    💣 (#126) - Remove patch endpoint

--- src/project/controllers/report.controller.ts
+++ src/project/controllers/report.controller.ts
@@ -15,7 +15,6 @@ import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/shared/decorators/roles.decorator';
 import { Role } from 'src/shared/enums/role.enum';
 import { CreateReportRequestDto } from '../dto/request/create-report.dto';
-import { ModifyReportRequestDto } from '../dto/request/modify-report.dto';
 import { ProjectParamsDto } from '../dto/request/project-params.dto';
 import { ReportService } from '../services/report.service';
 
@@ -38,18 +37,6 @@ export class ReportController {
   @UseGuards(JwtAuthGuard)
   async getReport(@Req() req: Request, @Param() param: ProjectParamsDto) {
     return this.reportService.getReport(req, param);
-  }
-
-  @Patch(':uuid/report')
-  @Roles(Role.User)
-  @UseGuards(RolesGuard)
-  @UseGuards(JwtAuthGuard)
-  async modifyReport(
-    @Req() req: Request,
-    @Param() param: ProjectParamsDto,
-    @Body() payload: ModifyReportRequestDto,
-  ) {
-    return this.reportService.modifyReport(req, param, payload);
   }
 
   @Delete(':uuid/report')
