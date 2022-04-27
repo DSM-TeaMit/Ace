2922e05c03020eaf5ed4d5309bfd795d1da5f526
pjp0704 <junhyeong25@gmail.com>
2022-04-27 13:47:07 +0900

    💫 (#189) - Change interface type

--- src/file/interfaces/uploadFileOptions.interface.ts
+++ src/file/interfaces/uploadFileOptions.interface.ts
@@ -2,8 +2,8 @@ export interface UploadFileOptions {
   file?: Express.MulterS3.File;
   files?: Express.MulterS3.File[];
   fileName?: string;
-  fileType: 'pdf' | 'video' | 'image' | 'archive';
+  fileType: 'pdf' | 'video' | 'image' | 'archive' | 'excel';
   folder?: string;
-  projectUuid: string;
+  projectUuid?: string;
   allowedExt: RegExp;
 }
