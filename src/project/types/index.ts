058370c84b1cbdb0ea1fcac87bdf42fe1558f53e
pjp0704 <junhyeong25@gmail.com>
2022-06-10 01:42:00 +0900

    ➕ (#28) - Add types

---
+++ src/project/types/index.ts
@@ -0,0 +1,12 @@
+export type RequestorType = 'USER_NON_EDITABLE' | 'USER_EDITABLE' | 'ADMIN';
+export type DocumentStatus =
+  | 'NOT_SUBMITTED'
+  | 'PENDING'
+  | 'ACCEPTED'
+  | 'REJECTED';
+export type ProjectStatus =
+  | 'PLANNING'
+  | 'PENDING(PLAN)'
+  | 'REPORTING'
+  | 'PENDING(REPORT)'
+  | 'DONE';
